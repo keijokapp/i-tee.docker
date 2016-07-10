0357bd1acad572debcb07b04d112c838cda71709
Keijo Kapp <keijo.kapp@gmail.com>
2016-07-10 17:53:44 +0000

    Made run-services.sh exit gracefully

--- fs/usr/local/bin/run-services.sh
+++ fs/usr/local/bin/run-services.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
+# Some shitty process manager
+# Vboxwebsrv & phpvirtualbox will probably be
+# removed or moved outside this container at
+# some point, so we will not need any process
+# manager in the future
+
 set -e
+
+terminate() {
+	set -e
+
+	echo "Caught SIGTERM. Terminating"
+
+	kill -TERM $VBOXWEBSRV_PID $APACHE2_PID
+	wait
+
+}
+
+trap "terminate" TERM
 
 /bin/sh /usr/local/bin/vboxwebsrv.sh &
 
+VBOXWEBSRV_PID=$!
+
 /bin/sh /usr/local/bin/apache2.sh &
+
+APACHE2_PID=$!
+
+set +e
 
 wait
