dc4eca7c89d8734d16384569d04d280c282071bb
Keijo Kapp <keijo.kapp@gmail.com>
2016-07-13 07:52:24 +0000

    Added cp production_sample.rb to checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -12,6 +12,9 @@ then
 
 	git reset --hard "origin/$ITEE_BRANCH"
 
+	cp /var/www/i-tee/config/environments/production_sample.rb \
+	  /var/www/i-tee/config/environments/production.rb
+
 fi
 
 echo "Using i-tee branch $ITEE_BRANCH"
