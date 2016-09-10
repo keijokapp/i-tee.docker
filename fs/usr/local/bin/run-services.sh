5674f2b28ef424bdf2cccaef4f8d7f90b753c710
Keijo Kapp <keijo.kapp@gmail.com>
2016-09-10 19:42:51 +0000

    Made small changes related to SSH

--- fs/usr/local/bin/run-services.sh
+++ fs/usr/local/bin/run-services.sh
@@ -3,6 +3,18 @@
 
 set -e
 
+ssh_master_client() {
+	while true
+	do
+		ssh vbox@host.local -N
+		sleep 1 # just in case
+	done
+}
+
+# run master SSH client instance to speed up SSH commands
+# using SSH connection multiplexing
+ssh_master_client &
+
 cd /var/www/i-tee
 
 exec /usr/local/bundle/bin/passenger start -p 80 -e production
